8d8a21d1ea50f31fa03b7ea9c4f5314b943d0334
perth pp <kekskcfm@gmail.com>
2020-09-04 22:45:44 +0700

    Add project files.

---
+++ week4/Source.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    char letter[10000];
+    int i = 0, n = 0, count = 0;
+    scanf_s("%s", letter,10000);
+
+    while (letter[i] != '\0')
+    {
+
+        count += 1;
+        if ((int)letter[i] - 'A' >= 22)
+        {
+            n += 1;
+        }
+        i++;
+    }
+    if (n == count)
+    {
+        printf("All Small Letter");
+    }
+    else if (n >= 1 && n <= count)
+    {
+        printf("Mix");
+    }
+    else
+        printf("All Capital Letter");
+    return 0;
+}
